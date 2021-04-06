eab88e06dd23a2c37bc3ab1b35a12c7401770eeb
lucasmetron <lucasmetron@gmail.com>
2021-04-06 15:01:23 -0300

    pegando atributo com jQuery

--- script.js
+++ script.js
@@ -4,6 +4,9 @@ console.log($('#lista1').html()); // Pega a tag inteira com o HTML
 
 console.log($('#novo').val()) // Pega os valores de inputs
 
+console.log($('#novo').attr('name')) // Pega atributos de tags ou inputs
+
+
 
 let teste = $('#lista1').children('.item2')[0]
 
