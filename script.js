41454cb531d355b8c112b336a3b49004cf458d44
lucasmetron <lucasmetron@gmail.com>
2021-04-06 13:38:50 -0300

    encadeando funções

--- script.js
+++ script.js
@@ -1,2 +1,17 @@
-console.log($('#lista1').find('.item2'))
-console.log($('body').children())
+let jq = $('#lista1');
+
+console.log(jq.slideUp(2000).slideDown(1000).fadeOut().fadeIn())
+
+let objeto = {
+    print10: function (){
+        console.log(10)
+        return this;
+    },
+
+    print20: function (){
+        console.log(20)
+        return this;
+    }
+}
+
+objeto.print10().print10().print20().print10()
