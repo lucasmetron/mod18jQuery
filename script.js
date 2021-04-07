4a2e64d5c04e2d1f0da5d2bdcde3bf29931b4ce9
lucasmetron <lucasmetron@gmail.com>
2021-04-07 10:50:26 -0300

    ex com retangulo

--- script.js
+++ script.js
@@ -1,5 +1,11 @@
 
-$('#lista1').append('<li class=item>Item 6</li>') //add item no final da lista
-$('#lista1').prepend('<li class=item >Item 0</li>') //add item no inicio da lista
-$('#lista1').before('<li class=item >Item 0</li>') // add item antes da UL lista1, ou seja fora da ul
-$('#lista1').after('<li class=item >Item 0</li>')// add item depois da UL lista1, ou seja fora da ul
+// $('#lista1').append('<li class=item>Item 6</li>') //add item no final da lista
+// $('#lista1').prepend('<li class=item >Item 0</li>') //add item no inicio da lista
+// $('#lista1').before('<li class=item >Item 0</li>') // add item antes da UL lista1, ou seja fora da ul
+// $('#lista1').after('<li class=item >Item 0</li>')// add item depois da UL lista1, ou seja fora da ul
+
+$('#lista1').append('<li> item 6</li>')
+
+let lis = document.getElementsByTagName('li')
+
+console.log(lis[5].className ='item item6')
