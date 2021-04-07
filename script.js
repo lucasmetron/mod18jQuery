d61895577d593790552fdb256789b05062c225cb
lucasmetron <lucasmetron@gmail.com>
2021-04-07 10:04:10 -0300

    como remover elementos

--- script.js
+++ script.js
@@ -1,5 +1,11 @@
 
-// console.log($('[key=2]').remove()) //  Selecionando a key por [] e depois removendo da lista
+console.log($('[key=2]').remove()) //  Selecionando a key por [] e depois removendo da lista
 
 
-$('#lista1').children('[key = 2]').remove()
+$('#lista1').children('[key = 2]').remove() 
+
+$('#lista1').remove() // Remove a lista toda
+
+$('#lista1').empty(); //A tag Ul ainda existe pois esta função só remove os filhos dela
+
+$('li').remove('.item2, .item3') //Selecionando todos os LIs, a função remove pode receber parametros para apagar item especificos
